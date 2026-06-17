6652473bb64562458e1f682d1a6835ad9d9c228e
Christian Chapman-Bird <c.chapmanbird@gmail.com>
2026-06-17 17:12:59 +0100

    waveform from dense solution

--- src/fewjaxexplore/summation/interpolatedsum.py
+++ src/fewjaxexplore/summation/interpolatedsum.py
@@ -1,6 +1,6 @@
 from jax import lax, Array
 import jax.numpy as jnp
-
+from diffrax import Solution
 
 def get_mode_sum_at_t(idx: int, dt: float, amplitude_coefficients: Array, phase_coefficients: Array) -> Array:
     c_a = amplitude_coefficients[idx]
@@ -42,6 +42,49 @@ def interpolated_summation_chunked(t_eval: Array, t_knots: Array, amplitude_coef
             idx_dt[0], idx_dt[1], amplitude_coefficients, phase_coefficients
             ),
         (indices, dt),
+        batch_size=batch_size
+    )
+    return out
+
+def get_mode_sum_at_t_odesol(t: float, idx: int, dt: float, amplitude_coefficients: Array, ode_solution: Solution, mode_indices: Array, Msec: float) -> Array:
+    c_a = amplitude_coefficients[idx]
+    amp   = c_a[0] + dt*(c_a[1] + dt*(c_a[2] + dt*c_a[3]))
+    phase = ((ode_solution.evaluate(t / Msec)[2:,None]) * mode_indices).sum(0)
+    co = jnp.cos(phase)
+    si = jnp.sin(phase)
+    return jnp.sum(amp * (co - 1j * si))
+
+def interpolated_summation_chunked_odesol(t_eval: Array, t_knots: Array, amplitude_coefficients: Array, ode_solution: Solution, mode_indices: Array, Msec: float, batch_size: int) -> Array:
+    """
+    Interpolated summation of modes at given evaluation times t_eval, using precomputed spline coefficients for amplitude and phase.
+    
+    The function processes the evaluation times in batches of size batch_size to manage memory usage, using `lax.map`. This is crucial!
+
+    Parameters
+    ----------
+    t_eval : Array
+        1D array of times at which to evaluate the waveform. Must be sorted in ascending order.
+    t_knots : Array
+        1D array of knot points corresponding to the spline coefficients. Must be sorted in ascending order.
+    amplitude_coefficients : Array
+        3D array of shape (num_modes, 4, max_length) containing the amplitude spline coefficients.
+    ode_solution : Solution
+        The solution object from the ODE solver, containing the interpolated state at each time.
+    batch_size : int
+        Size of batches to process evaluation times in. Must be static at runtime for JIT compilation.
+    
+    Returns
+    -------
+    Array
+        1D array of complex waveform values at each time in t_eval, computed by summing over modes using the interpolated amplitude and phase at each time.
+    """
+    indices = jnp.searchsorted(t_knots, t_eval, side="right") - 1
+    dt = t_eval - t_knots[indices]
+    out = lax.map(
+        lambda t_idx_dt: get_mode_sum_at_t_odesol(
+            t_idx_dt[0], t_idx_dt[1], t_idx_dt[2], amplitude_coefficients, ode_solution, mode_indices, Msec
+            ),
+        (t_eval, indices, dt),
         batch_size=batch_size
     )
     return out
--- src/fewjaxexplore/waveform.py
+++ src/fewjaxexplore/waveform.py
@@ -1,6 +1,6 @@
 from .amplitude.ylm import SpinWeightedSphericalHarmonic
 from .summation.spline import get_spline_coefficients_variable
-from .summation.interpolatedsum import interpolated_summation_chunked
+from .summation.interpolatedsum import interpolated_summation_chunked, interpolated_summation_chunked_odesol
 from .constants import MTSUN_SI, Gpc, MRSUN_SI
 
 import jax
@@ -30,6 +30,31 @@ def construct_amplitude_phase_splines(traj_gen, amp_gen, m1: float, m2: float, a
     phase_coefficients = jax.vmap(get_spline_coefficients_variable, in_axes=(None, 0, None, None))(t, phases, length, MAXLEN)
     return t, length, amplitude_coefficients.transpose(1, 2, 0), phase_coefficients.transpose(1, 2, 0)
 
+def construct_amplitudes_sol(traj_gen, amp_gen, m1: float, m2: float, a: float, p0: float, e0: float, Phi_phi0: float, Phi_r0: float, dist: float, theta: float, T: float, l_modes: Array, m_modes: Array, n_modes: Array) -> tuple[Array, Array, Array, Array]:
+    M = m1 + m2
+    Msec = M * MTSUN_SI
+    mu = m1 * m2 / M
+
+    ylms = jax.vmap(SpinWeightedSphericalHarmonic, in_axes=(0, 0, None, None))(l_modes, m_modes, theta, -jnp.pi/2)
+    sol = traj_gen(m1, m2, a, p0, e0, Phi_phi0, Phi_r0, T)
+    length = sol.stats["num_accepted_steps"]
+    MAXLEN = 256 + 1
+
+    t = sol.ts * Msec  # convert back to seconds
+    p, e, Phi_phi, Phi_r = sol.ys.T
+
+    amplitudes = (jax.vmap(amp_gen, in_axes=(None, 0, 0, None, None, None))(a, p, e, l_modes, m_modes, n_modes) * ylms[None, :]).T
+    amplitudes = amplitudes * (mu * MRSUN_SI  / dist / Gpc)
+
+    amplitude_coefficients = jax.vmap(get_spline_coefficients_variable, in_axes=(None, 0, None, None))(t, amplitudes, length, MAXLEN)
+    return t, length, amplitude_coefficients.transpose(1, 2, 0), sol
+
+
 def build_waveform_from_coefficients(t_eval, t_knots, amplitude_coefficients, phase_coefficients, batch_size):
     return interpolated_summation_chunked(
         t_eval, t_knots, amplitude_coefficients, phase_coefficients, batch_size)
+
+def build_waveform_from_coefficients_and_dense_solution(t_eval, t_knots, amplitude_coefficients, ode_solution, mode_indices, Msec, batch_size):
+    return interpolated_summation_chunked_odesol(
+        t_eval, t_knots, amplitude_coefficients, ode_solution, mode_indices, Msec, batch_size)
+        
